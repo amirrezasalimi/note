c2eb3283e2766326d43618d76f1e4199ed06a00a
seville <amirrezasalimi0@gmail.com>
2024-04-01 21:31:25 +0330

    Add Dockerfile and update Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   plugins: [react()],
-})
+});
